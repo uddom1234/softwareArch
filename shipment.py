f4065f9c05e1f7596254c791e9c8c2b351499a2d
UNF4Z3D21 <76532002+UNF4Z3D21@users.noreply.github.com>
2023-05-28 15:33:37 +1000

    Update shipment.py

--- shipment.py
+++ shipment.py
@@ -1,3 +1,10 @@
 class Shipment:
+    def __init__(self, order):
+        self.order = order
+        self.status = 'New'
+
     def update_status(self, status):
-        print("Shipment status:", status)
+        valid_statuses = ['New', 'Preparing', 'Shipped', 'Delivered']
+        if status not in valid_statuses:
+            raise Exception("Invalid shipment status.")
+        self.status = status
