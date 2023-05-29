bc41241f6fb0f86d11e1eca38c58ddbde687f55e
UNF4Z3D21 <76532002+UNF4Z3D21@users.noreply.github.com>
2023-05-29 23:04:22 +1000

    Update shipment.py

--- shipment.py
+++ shipment.py
@@ -1,8 +1,10 @@
 class Shipment:
+    # Constructor initializes the shipment with an order and sets its status to 'New'.
     def __init__(self, order):
         self.order = order
         self.status = 'New'
 
+    # Updates the shipment status if it's valid, otherwise raises an exception.
     def updateStatus(self, status):
         validStatuses = ['New', 'Preparing', 'Shipped', 'Delivered']
         if status not in validStatuses:
