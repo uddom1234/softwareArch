500452838736904d7ca6472b87248017c0faee74
UNF4Z3D21 <76532002+UNF4Z3D21@users.noreply.github.com>
2023-05-29 23:05:02 +1000

    Update orderManager.py

--- orderManager.py
+++ orderManager.py
@@ -2,9 +2,11 @@ from shipment import Shipment
 from shipmentProvider import ShipmentProvider
 
 class OrderManager:
+    # Constructor initializes the order manager with a shipment provider.
     def __init__(self):
         self.shipmentProvider = ShipmentProvider()
 
+    # Confirms the order if it's paid, creates a new shipment, and returns it.
     def confirmOrder(self, order):
         if order.status != 'Paid':
             raise Exception("Order must be paid before it can be confirmed.")
