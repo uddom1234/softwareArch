dcabb0a5906875a6686f4f72fe8b776459e021f5
UNF4Z3D21 <76532002+UNF4Z3D21@users.noreply.github.com>
2023-05-29 23:08:28 +1000

    Update shoppingCart.py

--- shoppingCart.py
+++ shoppingCart.py
@@ -1,12 +1,17 @@
 class ShoppingCart:
+    # Constructor function that initializes an empty list of items
     def __init__(self):
         self.items = []
 
+    # Function to add an item to the list of items
     def addItems(self, item):
         self.items.append(item)
 
+    # Function to remove an item from the list of items
     def deleteItems(self, item):
         self.items.remove(item)
+    
+    # Function to calculate the total cost of all items in the shopping cart
     def calculateTotalCost(self):
         totalCost = 0
         for item in self.items:
