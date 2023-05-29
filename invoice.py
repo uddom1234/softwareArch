263ffed71bcc50dc50a6f328b94dd44f1368d46e
Min He Ng <minhe0611@gmail.com>
2023-05-29 22:49:02 +1000

    Update invoice.py

--- invoice.py
+++ invoice.py
@@ -2,6 +2,7 @@ class Invoice:
     def __init__(self, order):
         self.order = order
 
+    # Generates an invoice for the order    
     def generateInvoice(self):
         response = ''
         response += "Invoice for: " + self.order.customer.name + "\n"
@@ -11,6 +12,7 @@ class Invoice:
             response += item.name + " $" + str(item.price)+ "\n"
         response += "Total: $" + str(self.order.total())+ "\n"
         response += "Payment Status: " + str(self.order.status)+ "\n"
+        # # Writes the invoice to a text file in the 'orders' directory
         with open(f'orders/order_{self.order.orderID}.txt', 'a') as f:
             f.write(response)
 
