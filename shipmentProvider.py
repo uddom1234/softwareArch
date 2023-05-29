ca9a9cf6b32748e6b5164ff9b5d04a5bf17a8b80
UNF4Z3D21 <76532002+UNF4Z3D21@users.noreply.github.com>
2023-05-29 23:03:46 +1000

    Update shipmentProvider.py

--- shipmentProvider.py
+++ shipmentProvider.py
@@ -1,14 +1,18 @@
 class ShipmentProvider:
+    # Constructor initializes shipments list and provider name.
     def __init__(self):
         self.shipments = []
         self.name = 'DHL'
 
+    # Adds a shipment and sets its status to 'Preparing'.
     def addShipment(self, shipment):
         self.shipments.append(shipment)
         shipment.updateStatus('Preparing')
 
+    # Updates a shipment's status. If status is 'Shipped', notifies the customer.
     def updateShipmentStatus(self, shipment, status):
         shipment.updateStatus(status)
         if status == 'Shipped':
             shipment.order.customer.notifyOrderShipped()
+
 
